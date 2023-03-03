71082bf154b9b84a03bd56c86d5c3a9fafe07ab9
Beth Massi <bethma@microsoft.com>
2023-03-03 15:27:22 -0800

    Update lottie to run in razor instead of xaml. Clean up unnecessary files.

--- BethTimeUntil/MauiProgram.cs
+++ BethTimeUntil/MauiProgram.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
 using SkiaSharp.Views.Maui.Controls.Hosting;
-using BethTimeUntil.Data;
 
 namespace BethTimeUntil;
 
@@ -23,8 +22,6 @@ public static class MauiProgram
 		builder.Services.AddBlazorWebViewDeveloperTools();
 		builder.Logging.AddDebug();
 #endif
-
-		builder.Services.AddSingleton<WeatherForecastService>();
 
 		return builder.Build();
 	}
