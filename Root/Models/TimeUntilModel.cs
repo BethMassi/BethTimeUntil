6373a9fb0af2f2a400accc2a580f55b9dae0cb06
Beth Massi <bethma@microsoft.com>
2023-03-15 16:21:40 -0700

    Update defaults

--- Root/Models/TimeUntilModel.cs
+++ Root/Models/TimeUntilModel.cs
@@ -11,7 +11,7 @@ namespace Root.Models
     {
         [Required]
         [StringLength(100, ErrorMessage = "Name is too long. Must be less than 100 characters.")]
-        public string Name { get; set; } = "Countdown to...";
+        public string Name { get; set; } = "My Countdown";
         [Required]        
         public DateTime CountdownDate { get; set; } = DateTime.Today.AddDays(7);
         public string AnimationImage { get; set; } = "lottie_hourglass";        
